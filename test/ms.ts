540b0b9ca46c1eb8fd713cb7f6b9eb76a9509a03
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-12-19 12:47:22 +0000

    #13 fix: ajustando o vercel.json para build e start em js

--- test/ms.ts
+++ test/ms.ts
@@ -1,8 +1,5 @@
-import ReqFunc from "../src/services/api/__reqfunc";
-
 async function main() {
     const start = Date.now()
-
 
     const duration = Date.now() - start
     console.log(`${duration}ms`)
